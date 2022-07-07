ca7f1d74271944a3706184292ad43bddfbffa0df
ayubch <ayoub.chahir96@gmail.com>
2022-07-07 08:41:04 +0100

    implement changes requested by code review

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User'
   belongs_to :post, counter_cache: :likes_counter
 
   # after_save :update_likes_count
