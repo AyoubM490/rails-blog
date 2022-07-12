ec0de46ead034ac6432ced219dcb134006e52e44
ayoub <chahayb563@gmail.com>
2022-07-12 23:29:52 +0100

    fix linter error

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   validates_presence_of :title
   validates_length_of :title, maximum: 250
   validates_numericality_of :likes_count, only_integer: true, greater_than_or_equal_to: 0
-  validates_numericality_of :comments_count, only_integer: true, greater_than_or_equal_to: 0 
+  validates_numericality_of :comments_count, only_integer: true, greater_than_or_equal_to: 0
 
   belongs_to :author, class_name: 'User',
                       foreign_key: 'author_id',
