67204f5f2ebea061cfe4e837f539aab3fc1a6bf4
ayoub <chahayb563@gmail.com>
2022-07-19 19:19:58 +0100

    Add devise gem and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  default_url_options :host => "localhost:3000"
+  devise_for :users
   root "users#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :create, :new, :show]
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,16 +1,16 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Like, type: :model do
   before do
-    @user = User.create(name: 'Jerry', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from US.')
-    @post = Post.new(user: @user, title: 'Hello1', text: 'This is my 1 post')
+    @user = User.create(name: "Jerry", photo: "https://unsplash.com/photos/F_-0BxGuVvo", bio: "Teacher from US.")
+    @post = Post.create(user: @user, title: "Hello1", text: "This is my 1 post")
   end
   subject { Like.create(post: @post, user: @user) }
 
-  describe 'after_save' do
+  describe "after_save" do
     before { subject.save }
 
-    it 'should have update the likes_count of the post' do
+    it "should have update the likes_count of the post" do
       expect(@post.likes_count).to eql(1)
     end
   end
