8cfb07758ac5bb595c7e2c7f8e1d8e7276f87663
ayoub <chahayb563@gmail.com>
2022-07-19 23:15:10 +0100

    - add name text_field - add :confirmable option to the devise options to let the app ask for confirmation for each email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :name, presence: true
   validates_numericality_of :posts_counter, only_integer: true, greater_than_or_equal_to: 0
 
-  has_many :posts, foreign_key: 'user_id', dependent: :destroy
-  has_many :likes, foreign_key: 'user_id', dependent: :destroy
-  has_many :comments, foreign_key: 'user_id', dependent: :destroy
+  has_many :posts, foreign_key: "user_id", dependent: :destroy
+  has_many :likes, foreign_key: "user_id", dependent: :destroy
+  has_many :comments, foreign_key: "user_id", dependent: :destroy
 
   def most_recent_three_posts
     Post.includes(:user).order(created_at: :desc).limit(3)
