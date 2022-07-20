bf8ff7e515ff0586e8e1a02d1f7ea30b5fa8e1ae
ayoub <chahayb563@gmail.com>
2022-07-20 19:29:39 +0100

    fix linter error

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,11 +1,9 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     user ||= User.new
-    if user.role == "admin"
+    if user.role == 'admin'
       can :manage, :all
     else
       can :manage, Post, user_id: user.id
